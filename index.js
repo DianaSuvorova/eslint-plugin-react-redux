fd1abf81bbf3789e8ffe2a780abde6cc7ff73403
Diana Suvorova <diana.suvorova@gmail.com>
2018-01-12 18:46:45 -0800

    feat: mapDispatchToProps-prefer-object

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const rules = {
   'connect-prefer-minimum-two-arguments': require('./lib/rules/connect-prefer-minimum-two-arguments'),
   'connect-prefer-named-arguments': require('./lib/rules/connect-prefer-named-arguments'),
-  'mapStateToProps-prefer-parameters-names': require('./lib/rules/mapStateToProps-prefer-parameters-names'),
+  'mapDispatchToProps-prefer-object': require('./lib/rules/mapDispatchToProps-prefer-object'),
   'mapDispatchToProps-prefer-parameters-names': require('./lib/rules/mapDispatchToProps-prefer-parameters-names'),
+  'mapStateToProps-prefer-parameters-names': require('./lib/rules/mapStateToProps-prefer-parameters-names'),
   'prefer-separate-component-file': require('./lib/rules/prefer-separate-component-file'),
 };
 
@@ -24,8 +25,9 @@ module.exports = {
       rules: {
         'react-redux/connect-prefer-minimum-two-arguments': 0,
         'react-redux/connect-prefer-named-arguments': 2,
-        'react-redux/mapStateToProps-prefer-parameters-names': 2,
         'react-redux/mapDispatchToProps-prefer-parameters-names': 2,
+        'react-redux/mapDispatchToProps-prefer-object': 2,
+        'react-redux/mapStateToProps-prefer-parameters-names': 2,
         'react-redux/prefer-separate-component-file': 1,
       },
     },
