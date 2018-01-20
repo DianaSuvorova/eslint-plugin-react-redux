bf0978ddadaf8cc588abb200371b376a566b9ebc
Diana Suvorova <diana.suvorova@uber.com>
2018-01-19 17:00:12 -0800

    fix: adding strict mode for nove v4,5

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,8 @@ module.exports = {
     rules: {
       "func-names": 0,
       "global-require": 0,
-      "prefer-destructuring": 0
+      "prefer-destructuring": 0,
+      "strict": 0
     },
     "env": {
       mocha: true
