c85383b54be200487d46fc3b14ce02ee7fc267f1
Diana Suvorova <diana.suvorova@uber.com>
2018-02-03 14:55:35 -0800

    fix: mapStateToProps-no-store support destructuring assignment

--- tests/lib/rules/mapStateToProps-no-store.js
+++ tests/lib/rules/mapStateToProps-no-store.js
@@ -57,6 +57,11 @@ ruleTester.run('mapStateToProps-no-store', rule, {
     'const mapStateToProps = (state) => {isActive: state.isActive}',
     `const mapStateToProps = (state, ownProps) => {};
       connect(mapStateToProps, null)(Alert);`,
+    `const mapStateToProps = ({ header }) => ({
+      isLoggedIn: header.user && header.user.isLoggedIn,
+    }); `,
+    'const mapStateToProps = ({header}, ownProps) => {header};',
+    'connect(({header}, ownProps) => {header})(App);',
   ],
   invalid: [{
     code: 'const mapStateToProps = (state) => state',
