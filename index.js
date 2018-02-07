7396174bfc49573cade40aab747be209ab90dfe3
DianaSuvorova <diana.suvorova@gmail.com>
2018-02-07 10:55:40 -0800

    feat: refactor MDTP-prefer-object to MDTP-returns-object (#15)
    * feat: refactor MDTP-prefer-object to MDTP-returns-object
    
    * fix: spelling

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const rules = {
   'connect-prefer-minimum-two-arguments': require('./lib/rules/connect-prefer-minimum-two-arguments'),
   'connect-prefer-named-arguments': require('./lib/rules/connect-prefer-named-arguments'),
-  'mapDispatchToProps-prefer-object': require('./lib/rules/mapDispatchToProps-prefer-object'),
+  'mapDispatchToProps-returns-object': require('./lib/rules/mapDispatchToProps-returns-object'),
   'mapDispatchToProps-prefer-parameters-names': require('./lib/rules/mapDispatchToProps-prefer-parameters-names'),
   'mapStateToProps-no-store': require('./lib/rules/mapStateToProps-no-store'),
   'mapStateToProps-prefer-parameters-names': require('./lib/rules/mapStateToProps-prefer-parameters-names'),
@@ -27,7 +27,7 @@ module.exports = {
         'react-redux/connect-prefer-minimum-two-arguments': 0,
         'react-redux/connect-prefer-named-arguments': 2,
         'react-redux/mapDispatchToProps-prefer-parameters-names': 2,
-        'react-redux/mapDispatchToProps-prefer-object': 2,
+        'react-redux/mapDispatchToProps-returns-object': 2,
         'react-redux/mapStateToProps-no-store': 2,
         'react-redux/mapStateToProps-prefer-parameters-names': 2,
         'react-redux/prefer-separate-component-file': 1,
