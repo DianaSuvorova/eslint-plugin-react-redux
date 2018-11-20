bcf596292d4ed56571d2ec5cf409d29ce02b4f3d
Diana Suvorova <diana.suvorova@uber.com>
2018-11-20 15:09:04 -0800

    fix: selectively filtering component props that are used in redux

--- tests/lib/rules/no-unused-prop-types.js
+++ tests/lib/rules/no-unused-prop-types.js
@@ -32,6 +32,22 @@ ruleTester.run('no-unused-prop-types', rule, {
     };
 
     export default connect(mapStateToProps)(MyComponent);`,
+
+    `export const mapDispatchToProps = (state, ownProps) => ({
+      myData: getMyData(state, ownProps.myProp),
+    });
+
+    export class MyComponent extends Component {
+      render() {
+        return <div>{this.props.myData}</div>;
+      }
+    }
+
+    MyComponent.propTypes = {
+      myProp: PropTypes.string.isRequired
+    };
+
+    export default connect(mapStateToProps)(MyComponent);`,
   ],
   invalid: [{
     code: `export const mapStateToProps = (state) => ({
@@ -53,6 +69,29 @@ ruleTester.run('no-unused-prop-types', rule, {
     errors: [
       {
         message: '\'myProp\' PropType is defined but prop is never used',
+      },
+    ],
+  }, {
+    code: `export const mapStateToProps = (state, ownProps) => ({
+      myData: getMyData(state, ownProps.myProp),
+    });
+
+    export class MyComponent extends Component {
+      render() {
+        return <div>{this.props.myData}</div>;
+      }
+    }
+
+    MyComponent.propTypes = {
+      myProp: PropTypes.string.isRequired,
+      notUsedProp:  PropTypes.string.isRequired,
+    };
+
+    export default connect(mapStateToProps)(MyComponent);`,
+
+    errors: [
+      {
+        message: '\'notUsedProp\' PropType is defined but prop is never used',
       },
     ],
   }],
